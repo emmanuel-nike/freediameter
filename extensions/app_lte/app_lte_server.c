0eaa7925782f2f7a035d6921051c78b8ccb809f1
Emmanuel Ikechukwu <emmanueln.nike@gmail.com>
2025-07-02 13:00:37 +0100

    updates to fix eap error on app_lte

--- extensions/app_lte/app_lte_server.c
+++ extensions/app_lte/app_lte_server.c
@@ -1267,13 +1267,13 @@ static int app_lte_answer_authorization_attributes(struct app_lte_state_machine 
 					fd_list_init(&ans_attrib->chain, NULL);
 					ans_attrib->attrib = auth_attrib->attrib;
 					if (toadd == 1)
-					{
-						app_lte_answer_set_attribute_valueA(&avp_attrib->value, &ans_attrib->tofree, avpdata.avp_basetype, &ans_attrib->value);
-					}
-					else
-					{
-						app_lte_answer_set_attribute_valueB(auth_attrib->value, &ans_attrib->tofree, avpdata.avp_basetype, &ans_attrib->value);
-					}
+					// {
+					// 	app_lte_answer_set_attribute_valueA(&avp_attrib->value, &ans_attrib->tofree, avpdata.avp_basetype, &ans_attrib->value);
+					// }
+					// else
+					// {
+					// 	app_lte_answer_set_attribute_valueB(auth_attrib->value, &ans_attrib->tofree, avpdata.avp_basetype, &ans_attrib->value);
+					// }
 					fd_list_insert_before(&app_lte_sm->ans_attributes,
 							&ans_attrib->chain);
 				}
